2d9b64f142ebe335a5b10fdec9d7478d38fa33c7
diegowanborges <diegowanborges@gmail.com>
2021-02-22 10:41:28 -0300

    Enrollment Seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -28,3 +28,6 @@ insert into tb_section(TITLE,DESCRIPTION,POSITION,IMG_URI,resource_id,prerequisi
 insert into tb_section(TITLE,DESCRIPTION,POSITION,IMG_URI,resource_id,prerequisite_id)values('Capitulo 2','Tags',2,'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg',1,1);
 insert into tb_section(TITLE,DESCRIPTION,POSITION,IMG_URI,resource_id,prerequisite_id)values('Capitulo 3','Divs',3,'https://cdn.pixabay.com/photo/2018/03/22/10/55/training-course-3250007_1280.jpg',1,2);
 
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (1, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+INSERT INTO tb_enrollment (user_id, offer_id, enroll_Moment, refund_Moment, available, only_Update) VALUES (2, 1, TIMESTAMP WITH TIME ZONE '2020-11-20T13:00:00Z', null, true, false);
+
